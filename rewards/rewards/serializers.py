6762bf96b06dea60bd6a5593889596e1c9e98ab6
Mike Hemelberg <mike@tradesy.com>
2021-02-09 13:17:04 -0600

    fixes

--- rewards/rewards/serializers.py
+++ rewards/rewards/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Transaction
+from rewards.models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
--- rewards/rewards/views.py
+++ rewards/rewards/views.py
@@ -4,3 +4,21 @@ from rest_framework.parsers import JSONParser
 from rewards.models import Transaction
 from rewards.serializers import TransactionSerializer
 
+@csrf_exempt
+def transaction_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Transaction.objects.all()
+        serializer = TransactionSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = TransactionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
